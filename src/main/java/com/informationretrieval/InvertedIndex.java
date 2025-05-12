0ef6fa555f59b12990591d3a8ef71f39f2f58f96
vincenciusadyatma <adyatmav@gmail.com>
2025-05-12 09:40:45 +0700

    feat: succes create calculation tf idf

--- src/main/java/com/informationretrieval/InvertedIndex.java
+++ src/main/java/com/informationretrieval/InvertedIndex.java
@@ -3,36 +3,38 @@ package com.informationretrieval;
 import java.util.HashMap;
 
 import java.util.Map;
-
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.List;
-
 public class InvertedIndex {
-    private Map<String,SortedSet<String>> invertedMap = new HashMap<>();
+  
     private DoclistOrdered<Term> invertedList = new DoclistOrdered<>();
 
-    public void addTerm(String docs, String[] tokens){
-        System.out.println(docs.substring(0,docs.lastIndexOf(".")) + " : " + tokens.length);
-        docs =  docs.substring(0,docs.lastIndexOf('.'));
-        for (String token : tokens) {
-            if (invertedMap.containsKey(token)) {
-                invertedMap.get(token).add(docs);
-            }else{
-                invertedMap.put(token, new TreeSet<>());
-                invertedMap.get(token).add(docs);
-            }
+    public void addTerm(Term term){
+        this.invertedList.addSort(term);
+    }
+
+    public Map<String,Map<String,Double>> computeTFIDF(){
+        Map<String,Map<String,Double>> matriks = new HashMap<>();
+        for (Term term : invertedList) {
+          matriks.put(term.getName(), new HashMap<>() );
+          for (Document doc : term.getDocList()) {
+            double tfidf = doc.getTf() * term.getIdf();
+            matriks.get(term.getName()).put(doc.getName(), tfidf);
+          }
         }
+        return matriks;
     }
 
-    public void addTerm(List<Term> termList){
-        for (Term term : termList) {
-            this.invertedList.addSort(term);
-        }
+    public double computeTFIDFquery(){
+         Map<String,Map<String,Double>> matriks = new HashMap<>();
+        return 0;
     }
 
-    public Map<String,SortedSet<String>> getMap(){
-        return this.invertedMap;
-    }
+    
 
+ 
+    public DoclistOrdered<Term> getInvertedList() {
+      return this.invertedList;
+    }
+    public void setInvertedList(DoclistOrdered<Term> value) {
+      this.invertedList = value;
+    }
 }
--- src/main/java/com/informationretrieval/Main.java
+++ src/main/java/com/informationretrieval/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
      
         ReadFile readCorpus = new ReadFile("documents");
-        System.out.println(readCorpus.read().get(14).getIdf());
-        System.out.println( readCorpus.read().get(14).getDocList().get(2).getTf());
+        InvertedIndex invertedListCorpus = readCorpus.read();
+        System.out.println(invertedListCorpus.computeTFIDF());
         // readCorpus.printMap();
         // DoclistOrdered<String> ordered1 = new DoclistOrdered<>();
      
